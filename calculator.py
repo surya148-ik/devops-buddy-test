5afec1bfe85721c12b5ea412fbb85d88caba95f7
surya148-ik <opsurya148@gmail.com>
2026-04-23 15:22:12 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,10 +1,24 @@
-import numpy as np
+import statistics
 
 def add(a, b):
-    return int(np.add(a, b))
+    return a + b
 
 def subtract(a, b):
-    return int(np.subtract(a, b))
+    return a - b
 
 def multiply(a, b):
-    return int(np.multiply(a, b))
+    return a * b
+
+def average(numbers):
+    return statistics.mean(numbers)
+
+def std_deviation(numbers):
+    return statistics.stdev(numbers)
+
+def get_summary(numbers):
+    return {
+        "mean": statistics.mean(numbers),
+        "median": statistics.median(numbers),
+        "stdev": statistics.stdev(numbers),
+        "variance": statistics.variance(numbers)
+    }
